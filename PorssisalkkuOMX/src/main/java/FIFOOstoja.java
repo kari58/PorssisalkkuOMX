4f0a1b54096f26f3e79f475f21e964549873c7c0
Romanowski Kari I <kromanow@da1-cs-bk107-29.helsinki.fi>
2014-09-12 12:24:38 +0300

    lisää

---
+++ PorssisalkkuOMX/src/main/java/FIFOOstoja.java
@@ -0,0 +1,58 @@
+
+import java.util.Calendar;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kromanow
+ */
+public class FIFOOstoja {
+    private String osakkeennimi;
+    private Paivamaara ostopaiva;
+    
+    public FIFOOstoja(String osakkeennimi,int paiva,int kuukausi,int vuosi){
+        this.osakkeennimi=osakkeennimi;
+        this.ostopaiva=new Paivamaara(paiva,kuukausi, vuosi);
+    }
+    public FIFOOstoja(String osakkeennimi,Paivamaara ostopaiva){
+        this.osakkeennimi=osakkeennimi;
+        this.ostopaiva=ostopaiva;
+    }
+    public FIFOOstoja(String osakkeennimi){
+        this.osakkeennimi=osakkeennimi;
+    
+    Calendar nyt=Calendar.getInstance();
+    int vuosi=nyt.get(Calendar.YEAR);
+    int kuukausi=nyt.get(Calendar.MONTH)+1;
+    int paiva=nyt.get(Calendar.DATE);
+    this.ostopaiva=new Paivamaara(paiva, kuukausi, vuosi);
+    
+}
+    public int vanhinosake(){   
+        Calendar nyt=Calendar.getInstance();
+    int vuosi=nyt.get(Calendar.YEAR);
+    int kuukausi=nyt.get(Calendar.MONTH)+1;
+    int paiva=nyt.get(Calendar.DATE);
+    Paivamaara tamaPaiva=new Paivamaara(paiva,kuukausi, vuosi);
+    return ostopaiva.erotusVuosissa(tamaPaiva);
+        
+    }
+    public boolean vanhempiKuin(FIFOOstoja verrattava){
+        return this.ostopaiva.aiemmin(verrattava.ostopaiva);
+    }
+    public String getOsakkeennimi(){
+        return this.osakkeennimi;
+    }
+    public String toString(){
+        return this.osakkeennimi+",ostopäivä "+this.ostopaiva;
+    }
+}
+//public class Main{
+   // public static void main(String[]args){
+      //  FIFOOstoja upm= new FIFOOstoja("UPM", 15,9,2014)
+  //  }
---
+++ PorssisalkkuOMX/src/main/java/Paivamaara.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kromanow
+ */
+public class Paivamaara {
+
+    private int paiva;
+    private int kuukausi;
+    private int vuosi;
+  
+
+    public Paivamaara(int paiva,int kuukausi,int vuosi) {
+        this.paiva = paiva;
+        this.kuukausi = kuukausi;
+        this.vuosi = vuosi;
+    }
+
+    public String toString() {
+        return this.paiva + "." + this.kuukausi + "." + this.vuosi;
+    }
+
+    public boolean aiemmin(Paivamaara verrattava) {
+        if (this.vuosi < verrattava.vuosi) {
+            return true;
+        }
+        if (this.vuosi == verrattava.vuosi && this.kuukausi < verrattava.kuukausi) {
+            return true;
+        }
+        if (this.vuosi == verrattava.vuosi && this.kuukausi == verrattava.kuukausi && this.paiva < verrattava.paiva) {
+            return true;
+        }
+        return false;
+
+    }
+
+    public int erotusVuosissa(Paivamaara verrattava) {  //vanhin ajassa pitäisi myös saada
+        if (aiemmin(verrattava)) {
+            return verrattava.erotusVuosissa(verrattava); // (this)
+        }
+        return laskeErotus(verrattava);
+    }
+
+    private int laskeErotus(Paivamaara verrattava) {
+        int vuosiPois = 0;
+        if (this.kuukausi < verrattava.kuukausi) {
+            vuosiPois = 1;
+        } else if (this.kuukausi ==verrattava.kuukausi 
+             && this.paiva < verrattava.paiva)
+        
+        
+            {
+        vuosiPois = 1;
+        }
+            return this.vuosi-verrattava.vuosi-vuosiPois;
+    }
+}
