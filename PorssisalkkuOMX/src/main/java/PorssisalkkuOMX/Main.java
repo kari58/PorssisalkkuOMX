faebc0272232c4117c0a6e65df1fdda0a09bbdc2
Romanowski Kari I <kromanow@da1-cs-bk107-31.helsinki.fi>
2014-10-16 14:34:44 +0300

    viimeisin 1

--- PorssisalkkuOMX/src/main/java/PorssisalkkuOMX/Main.java
+++ PorssisalkkuOMX/src/main/java/PorssisalkkuOMX/Main.java
@@ -10,6 +10,8 @@ import java.util.Scanner;
 import javax.swing.SwingUtilities;
 
 /*
+
+* testitesti
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
